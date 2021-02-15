026a9275b4e7ff74a8930e67d8e50aae6359bafa
Mitch Harris <mitchharris@live.ca>
2021-02-15 15:49:00 -0800

    set up:

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+This module puts everything together and is the entry point
+"""
+
+import messanger
+
+if __name__ == "__main__":
+    print("poop")
---
+++ messanger.py
@@ -0,0 +1,10 @@
+"""
+This module manages the functionality for sending sms's
+"""
+
+import os
+from twilio.rest import Client
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
