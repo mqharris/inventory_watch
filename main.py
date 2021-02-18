2963a4cfc57f599aa264c94f948d682a72429e64
Mitch Harris <mitchharris@live.ca>
2021-02-17 22:11:33 -0800

    add while loop and sleep

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 """
 This module puts everything together and is the entry point
 """
+import time
 
 import searcher
 import messanger
 
 if __name__ == "__main__":
-    try:
-        resp = searcher.get_response_from_site()
-        print_code = searcher.check_stock_status(resp)
-        message_text, recipients = messanger.send_text(resp, print_code)
-        print(message_text, recipients)
-    except Exception as error:
-        messanger.error_sender(error)
+    while True:
+        try:
+            resp = searcher.get_response_from_site()
+            print_code = searcher.check_stock_status(resp)
+            message_text, recipients = messanger.send_text(resp, print_code)
+            print(message_text, recipients)
+        except Exception as error:
+            messanger.error_sender(error)
+        if print_code == 0:
+            break
+        time.sleep(300)
